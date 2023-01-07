f809493cd39d6f472986720697b67645d56cd909
vacuum_packed <kontakt@akolod.de>
2023-01-07 16:09:40 +0100

    fixed gitignore

--- Assets/_ldjam/Scripts/PlayerController.cs
+++ Assets/_ldjam/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,11 +9,12 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private GameObject placeableObjectPrefab;
 
+    Camera _camera;
     private GameObject _placebleObjectInstance;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _camera = Camera.main;
     }
     
     // Update is called once per frame
@@ -28,6 +30,13 @@ public class PlayerController : MonoBehaviour
 
     public void OnLook(InputAction.CallbackContext context)
     {
+        var mousePosition = context.ReadValue<Vector2>();
+        
+        mousePosition = _camera.ScreenToWorldPoint(mousePosition);
+        var ray = new Ray(mousePosition, Vector3.down);
+        if (Physics.Raycast(ray, out var raycastHit, Single.PositiveInfinity, LayerMask.NameToLayer("Floor")))
+        {
+        }
         
     }
 }
