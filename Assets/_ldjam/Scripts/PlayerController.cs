d0c4ed0c6ba0f095f435e54edbbdaf926964f73c
vacuum_packed <kontakt@akolod.de>
2023-01-07 15:52:49 +0100

    added input and some tweaks

---
+++ Assets/_ldjam/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject placeableObjectPrefab;
+
+    private GameObject _placebleObjectInstance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnFire(InputAction.CallbackContext context)
+    {
+        
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        
+    }
+}
