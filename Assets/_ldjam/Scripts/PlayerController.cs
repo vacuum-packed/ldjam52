60d8ae65fd47c68de1723155f52f8104b8c866ab
D4KU <d4k@tuta.io>
2023-01-08 19:13:46 +0100

    Clamped stride to radius

--- Assets/_ldjam/Scripts/PlayerController.cs
+++ Assets/_ldjam/Scripts/PlayerController.cs
@@ -15,7 +15,7 @@ public class PlayerController : MonoBehaviour
     private Transform indicator;
 
     [SerializeField]
-    private Transform radiusIndicator;
+    private Transform pivot;
 
     [SerializeField]
     private float startFootDistance = .1f;
@@ -29,46 +29,77 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private VisualEffect effect;
 
+    private bool _leftFootPlaced = true;
+    private Camera _camera;
+    private bool _isOnLayer;
+    private int _layer;
+    private bool _scrolled;
+    private readonly Vector3 _outOfBoundsPosition = new(0, 100, 0);
+
+    [Header("Listening Events")]
+    [SerializeField]
+    private VoidEventChannelSO roundStartedEvent;
+
     [Header("Broadcasting Events")]
     [SerializeField]
     private BoolEventChannelSO isPositionValidEvent;
 
-    private Transform _lastFootPlaced;
-    private bool _leftFootPlaced = true;
-    private float _maxFoodDistanceSqr;
-    private Camera _camera;
-    private bool _isPositionValid;
-    private int _layer;
-    private readonly Vector3 _outOfBoundsPosition = new(0, 100, 0);
+    private void OnEnable()
+    {
+        roundStartedEvent.onEventRaised += OnRoundStarted;
+    }
+
+    private void OnDisable()
+    {
+        roundStartedEvent.onEventRaised -= OnRoundStarted;
+    }
 
     private void Start()
     {
         _camera = Camera.main;
         _layer = LayerMask.NameToLayer("Floor");
-        leftFoot.localPosition = Vector3.right * -startFootDistance;
-        rightFoot.localPosition = Vector3.right * startFootDistance;
-        _maxFoodDistanceSqr = maxFootDistance * maxFootDistance;
     }
 
     private void Update()
     {
+        if (Input.mouseScrollDelta.y == 0)
+            return;
+
+        _scrolled = true;
         float yRot = indicator.localEulerAngles.y;
         yRot += Input.mouseScrollDelta.y * scrollSensitivity;
         indicator.localRotation = Quaternion.AngleAxis(yRot, Vector3.up);
     }
 
+    private void OnRoundStarted()
+    {
+        leftFoot.localPosition = Vector3.right * -startFootDistance;
+        leftFoot.localRotation = Quaternion.identity;
+        rightFoot.localPosition = Vector3.right * startFootDistance;
+        rightFoot.localRotation = Quaternion.identity;
+        pivot.localPosition = Vector3.zero;
+    }
+
     public void OnFire(InputAction.CallbackContext context)
     {
-        if (context.action.phase != InputActionPhase.Canceled || !_isPositionValid)
+        if (context.action.phase != InputActionPhase.Canceled || !_isOnLayer)
             return;
 
-        _lastFootPlaced = _leftFootPlaced ? rightFoot : leftFoot;
-        _lastFootPlaced.SetPositionAndRotation(indicator.position, indicator.rotation);
-        Vector3 avgPos = (leftFoot.localPosition + rightFoot.localPosition) * .5f;
-        avgPos.y = radiusIndicator.localPosition.y;
-        radiusIndicator.localPosition = avgPos;
+        // Handle foot
+        Transform foot = _leftFootPlaced ? rightFoot : leftFoot;
+        foot.localPosition = indicator.localPosition;
+        foot.localRotation = indicator.localRotation;
 
+        // Handle radius indicator
+        Vector3 avgPos = (leftFoot.localPosition + rightFoot.localPosition) * .5f;
+        avgPos.y = pivot.localPosition.y;
+        pivot.localPosition = avgPos;
+
+        // Handle vfx
+        effect.transform.localPosition = indicator.localPosition;
         effect.Play();
+
+        _scrolled = false;
         _leftFootPlaced = !_leftFootPlaced;
     }
 
@@ -79,16 +110,21 @@ public class PlayerController : MonoBehaviour
         if (!Physics.Raycast(ray, out var raycastHit, 10000))
             return;
 
+        // Clamp strid
         Vector3 hitPosition = raycastHit.point;
-        indicator.position = hitPosition;
+        Vector3 lookDir = raycastHit.point - pivot.position;
+        Vector3 stride = Vector3.ClampMagnitude(lookDir, maxFootDistance);
+        Vector3 footPosition = pivot.position + stride;
 
-        bool valid = raycastHit.transform.gameObject.layer == _layer && IsInRadius(hitPosition);
-        _isPositionValid = valid;
-        isPositionValidEvent.RaiseEvent(valid);
-    }
+        // Raycast again with clamped position
+        ray = _camera.ScreenPointToRay(_camera.WorldToScreenPoint(footPosition));
+        Physics.Raycast(ray, out raycastHit, 10000);
 
-    private bool IsInRadius(Vector3 position)
-    {
-        return (radiusIndicator.position - position).sqrMagnitude <= _maxFoodDistanceSqr;
+        indicator.position = footPosition;
+        if (!_scrolled)
+            indicator.localRotation = Quaternion.LookRotation(lookDir, Vector3.up);
+
+        _isOnLayer = raycastHit.transform.gameObject.layer == _layer;
+        isPositionValidEvent.RaiseEvent(_isOnLayer);
     }
 }
